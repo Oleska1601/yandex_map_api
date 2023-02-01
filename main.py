dca3f47eff3e789b4956c8bdb1cb8ade44ac32b4
Oleska1601 <118464712+Oleska1601@users.noreply.github.com>
2023-02-01 22:04:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,88 @@
 import os
 import sys
-
 import requests
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMainWindow
-from map import Ui_MainWindow
+from PyQt5 import uic, QtCore
+from PyQt5.QtGui import QPixmap, QWheelEvent
+from PyQt5.QtWidgets import QApplication,  QMainWindow
 
-SCREEN_SIZE = [600, 450]
+SCREEN_SIZE = [600, 600]
 
 
-class StaticMap(QMainWindow, Ui_MainWindow):
+class StaticMap(QMainWindow):
+    current_coord = (37.530887, 55.703118)
+    current_spn = (0.002, 0.002)
+    current_map = 'map'
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
 
     def __init__(self):
         super().__init__()
-        self.getImage()
         self.initUI()
-        self.setupUi(self)
+        self.getImage()
 
     def getImage(self):
-
-        self.current_ll = (37.530887, 55.703118)
-        self.current_spn = (0.002, 0.002)
-        self.type = 'map'
-        params = {
-            "ll": ",".join(map(str, self.current_ll)),
-            "spn": ",".join(map(str, self.current_ll)),
-            "l": self.type
+        map_params = {
+            "ll": ",".join(map(str, self.current_coord)),
+            "spn": ",".join(map(str, self.current_spn)),
+            "l": self.current_map
         }
-        self.map_api_server = "http://static-maps.yandex.ru/1.x/"
-        response = requests.get(self.map_api_server, params)
+        response = requests.get(self.map_api_server, map_params)
 
         if not response:
             print("Ошибка выполнения запроса:")
             print(self.map_api_server)
             print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
 
         # Запишем полученное изображение в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
 
     def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
+        uic.loadUi('map.ui', self)
 
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.setPixmap(self.pixmap)
+    def wheelEvent(self, event: QWheelEvent) -> None:
+        if event.angleDelta().y() > 0:
+            self.changeMapScale('-')
+        else:
+            self.changeMapScale('+')
+        self.getImage()
+
+    def changeMapScale(self, eventScaleType: str):
+        current_change = 1
+        if eventScaleType == '-':
+            current_change = -current_change
+        new_spn = (self.current_spn[0] + current_change, self.current_spn[1] + current_change)
+        if 0 <= new_spn[0] <= 90 and 0 <= new_spn[1] <= 90:
+            self.current_spn = new_spn
+
+    def changeMapCenterPoint(self, event_change_type: str):
+        if event_change_type == 'up':
+            current_delta = (0, 1)
+            new_coord = (self.current_coord[0] + current_delta[0], self.current_coord[1] + current_delta[1])
+        elif event_change_type == 'down':
+            current_delta = (0, -1)
+            new_coord = (self.current_coord[0] + current_delta[0], self.current_coord[1] + current_delta[1])
+        elif event_change_type == 'left':
+            current_delta = (-1, 0)
+            new_coord = (self.current_coord[0] + current_delta[0], self.current_coord[1] + current_delta[1])
+        elif event_change_type == 'right':
+            current_delta = (1, 0)
+            new_coord = (self.current_coord[0] + current_delta[0], self.current_coord[1] + current_delta[1])
+        if 0 <= new_coord[0] <= 90 and 0 <= new_coord[1] <= 180:
+            self.current_coord = new_coord
+
+    def keyReleaseEvent(self, event):
+        if event.key() == QtCore.Qt.Key.Key_Up:
+            self.changeMapCenterPoint('up')
+        if event.key() == QtCore.Qt.Key.Key_Down:
+            self.changeMapCenterPoint('down')
+        if event.key() == QtCore.Qt.Key.Key_Left:
+            self.changeMapCenterPoint('left')
+        if event.key() == QtCore.Qt.Key.Key_Right:
+            self.changeMapCenterPoint('right')
+        self.getImage()
 
     def closeEvent(self, event):
         """При закрытии формы подчищаем за собой"""
